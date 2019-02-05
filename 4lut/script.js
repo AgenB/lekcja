179434789c402e66dc6782eb873a22b3a422082c
Gość <belicja1@gmail.com>
2019-02-05 11:21:21 +0100

    added style and deletion

--- 4lut/script.js
+++ 4lut/script.js
@@ -13,26 +13,37 @@
 
 const getData = {
     getBtn: document.querySelector(".getSWAPI"),
-    //swList: document.querySelector(".swList"),
+    swList: document.querySelector(".swList"),
 
     getAllFromSW: function() {
+        if (this.swList.querySelectorAll("li").length != 0) {
+            return;
+        }
         fetch("https://swapi.co/api/people/").then(function(response) {
             return response.json();
         }).then(function(responseUnpacked) {
             let dane = responseUnpacked.results;
-            swList = document.querySelector(".swList");
             dane.forEach(function(hero) {
                 console.log(hero.name);
                 let li = document.createElement("li");
                 li.innerText = hero.name;
-
+                li.className = "item";
+                let closeBtn = document.createElement("div");
+                closeBtn.className = "closeBtn";
                 this.swList.appendChild(li);
-            });
+                li.appendChild(closeBtn);
+            }, getData);
         });
+    },
+    deleteFromList: function(event) {
+        if (event.target.classList.contains("closeBtn")) {
+            event.target.parentElement.remove();
+        }
     },
 
     init: function() {
-        this.getBtn.addEventListener("click", this.getAllFromSW.bind(this));
+        this.getBtn.addEventListener("click", this.getAllFromSW.bind(getData));
+        this.swList.addEventListener("click", this.deleteFromList.bind(getData));
     }
 }
 
