7e9af475a03df592a2c6ba71b5fe3e5ff3ed0cc9
Gość <belicja1@gmail.com>
2019-02-05 11:27:36 +0100

    small upate

--- 4lut/script.js
+++ 4lut/script.js
@@ -1,16 +1,3 @@
-// fetch("https://swapi.co/api/people/").then(function(response) {
-//     return response.json();
-// }).then(function(responseUnpacked) {
-//     let dane = responseUnpacked.results;
-//     dane.forEach(function(hero) {
-//         console.log(hero.name);
-//         let li = document.createElement("li");
-//         li.innerText = hero.name;
-
-//         document.querySelector(".swList").appendChild(li);
-//     })
-// })
-
 const getData = {
     getBtn: document.querySelector(".getSWAPI"),
     swList: document.querySelector(".swList"),
