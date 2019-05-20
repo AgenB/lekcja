823d5907aeb5d0f5a089ed0e77e7ab8467726abe
Gość <belicja1@gmail.com>
2019-05-20 14:06:38 +0200

    game update

--- lekcjaMaj16/script.js
+++ lekcjaMaj16/script.js
@@ -6,16 +6,21 @@ const GAME_WIDTH = 640;
 const GAME_HEIGHT = 480;
 
 const PLAYER_WIDTH = 20;
+const MISSILE_MAX_SPEED = 1000;
 
 const GAME_STATE = {
-    lastTime: 0,
+    lastTime: Date.now(),
     leftPressed: false,
     rightPressed: false,
     spacePressed: false,
+    missileShot: false,
     playerX: 0,
     playerY: 0,
     missiles: []
 }
+
+const $container = document.querySelector(".game");
+const $playerDOM = document.createElement("img");
 
 function clamp(v, min, max) {
     if (v < min) {return min}
@@ -33,19 +38,26 @@ function createPlayer($container) {
     GAME_STATE.playerX = GAME_WIDTH/2;
     GAME_STATE.playerY = GAME_HEIGHT-50;
 
-    const $player = document.createElement("img");
-    $player.src = "img/spaceShips_004.png";
-    $player.className = "player";
-    $container.appendChild($player);
-    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+    $playerDOM.src = "img/spaceShips_004.png";
+    $playerDOM.className = "player";
+    $container.appendChild($playerDOM);
+    setPosition($playerDOM, GAME_STATE.playerX, GAME_STATE.playerY);
 }
 
 function updatePlayer() {
     if (GAME_STATE.leftPressed) {GAME_STATE.playerX -= 5}
     else if (GAME_STATE.rightPressed) {GAME_STATE.playerX += 5}
     GAME_STATE.playerX = clamp(GAME_STATE.playerX, PLAYER_WIDTH, GAME_WIDTH - PLAYER_WIDTH);
-    const $player = document.querySelector(".player");
-    setPosition($player, GAME_STATE.playerX, GAME_STATE.playerY);
+    if (GAME_STATE.spacePressed) {
+        if (!GAME_STATE.missileShot) {
+            createMissile($container, GAME_STATE.playerX, GAME_STATE.playerY);
+            GAME_STATE.missileShot = true;
+        }
+    }
+    else {
+        GAME_STATE.missileShot = false;
+    }
+    setPosition($playerDOM, GAME_STATE.playerX, GAME_STATE.playerY);
 }
 
 
@@ -62,7 +74,7 @@ function createMissile($container, x, y) {
     setPosition($missile, x, y);
 }
 
-function updateMissile() {
+function updateMissile(dt) {
     const missiles = GAME_STATE.missiles;
     missiles.map(missile => {
         missile.y -= MISSILE_MAX_SPEED * dt;
@@ -75,14 +87,12 @@ function update() {
     const currentTime = Date.now();
     const dt = (currentTime - GAME_STATE.lastTime) / 1000;
     updatePlayer();
-    if (GAME_STATE.spacePressed) {createMissile($container, GAME_STATE.playerX, GAME_STATE.playerY)}
-    updateMissile();
+    updateMissile(dt);
     GAME_STATE.lastTime = currentTime;
     window.requestAnimationFrame(update);
 }
 
 function init() {
-    const $container = document.querySelector(".game");
     createPlayer($container);
 }
 
