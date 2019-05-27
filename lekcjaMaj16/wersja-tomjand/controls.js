e796fb56619e21004f2b15f7e8850ed9ac070f99
Gość <belicja1@gmail.com>
2019-05-27 13:43:07 +0200

    updated tomjand version

--- lekcjaMaj16/wersja-tomjand/controls.js
+++ lekcjaMaj16/wersja-tomjand/controls.js
@@ -1,32 +1,36 @@
 import { GAME_STATE, KEY,BtnGameOver } from "./global.js";
-import { init } from "./scripts.js";
-
+import { init } from "./scripts.js"
+import { createPlayer } from "./player.js";
 
 export function onKeyDown(e) {
-     if (e.keyCode === KEY.LEFT) {
-        GAME_STATE.leftPressed = true;
-    } else if (e.keyCode === KEY.RIGHT) {
-        GAME_STATE.rightPressed = true;
-    } else if (e.keyCode === KEY.SPACE) {
-        GAME_STATE.spacePressed = true;
-    }
+  if (e.keyCode === KEY.LEFT) {
+    GAME_STATE.leftPressed = true;
+  } else if (e.keyCode === KEY.RIGHT) {
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY.SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
 }
 
 export function onKeyUp(e) {
-    if (e.keyCode === KEY.LEFT) {
-        GAME_STATE.leftPressed = false;
-    } else if (e.keyCode === KEY.RIGHT) {
-        GAME_STATE.rightPressed = false;
-    } else if (e.keyCode === KEY.SPACE) {
-        GAME_STATE.spacePressed = false;
-        GAME_STATE.shoot = true;
-    }
+  if (e.keyCode === KEY.LEFT) {
+    GAME_STATE.leftPressed = false;
+  } else if (e.keyCode === KEY.RIGHT) {
+    GAME_STATE.rightPressed = false;
+  } else if (e.keyCode === KEY.SPACE) {
+    GAME_STATE.spacePressed = false;
+    GAME_STATE.shoot = true;
+  }
 }
 
 
 
 window.addEventListener("keydown", onKeyDown);
 window.addEventListener("keyup", onKeyUp);
-BtnGameOver.addEventListener("click", () => {
-	init();
+
+BtnGameOver.addEventListener("click", (el) => {
+	console.log("GAME OVER");
+  GAME_STATE.gameOver = false;
+  el.target.style.display = 'none' ;
+  init();
 })
