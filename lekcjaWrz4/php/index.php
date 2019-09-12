a2d778a3d91feae11d365815f576046a7498f408
Gość <belicja1@gmail.com>
2019-09-12 14:55:02 +0200

    update

--- lekcjaWrz4/php/index.php
+++ lekcjaWrz4/php/index.php
@@ -10,67 +10,7 @@
     <div class="container">
         <div class="header">header</div>
         <div class="sidebar">sidebar</div>
-        <!-- <div class="item1 card">
-            <div class="product">
-                <img src="http://www.idealhomeshowshop.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/d/adjustable-mesh-office-swivel-chair.jpg"
-                class="product-img" alt="office chair">
-                <div class="product-info">
-                    <p class="product-price">Cena: <span class="product-price-value">130zł</span><br>
-                    <span class="product-sale">Promocja</span></p>
-                    <div class="product-variants">Kolor:
-                        <ul>
-                            <li>Czarny</li>
-                            <li>Szary</li>
-                            <li>Beżowy</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="product-desc">
-                    <h3>Krzesło Biurowe</h3>
-                    <p>Obrotowe i gładko jeździ</p>
-                    <a href="#">Więcej</a>
-                    <a href="#">Kup Teraz</a>
-                </div>
-            </div>
-        </div> -->
-        <?php
-            for ($i = 0; $i < 12; $i++) {
-                echo('<div class="item card">
-                    <div class="product">
-                        <img src="http://www.idealhomeshowshop.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/d/adjustable-mesh-office-swivel-chair.jpg"
-                        class="product-img" alt="office chair">
-                        <div class="product-info">
-                            <p class="product-price">Cena: <span class="product-price-value">130zł</span><br>
-                            <span class="product-sale">Promocja</span></p>
-                            <div class="product-variants">Kolor:
-                                <ul>
-                                    <li>Czarny</li>
-                                    <li>Szary</li>
-                                    <li>Beżowy</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="product-desc">
-                            <h3>Krzesło Biurowe</h3>
-                            <p>Obrotowe i gładko jeździ</p>
-                            <a href="#">Więcej</a>
-                            <a href="#">Kup Teraz</a>
-                        </div>
-                    </div>
-                </div>');
-            }
-        ?>
-        <!-- <div class="item2 card">2</div>
-        <div class="item3 card">3</div>
-        <div class="item4 card">4</div>
-        <div class="item5 card">5</div>
-        <div class="item6 card">6</div>
-        <div class="item7 card">7</div>
-        <div class="item8 card">8</div>
-        <div class="item9 card">9</div>
-        <div class="item10 card">10</div>
-        <div class="item11 card">11</div>
-        <div class="item12 card">12</div> -->
+        <?php require("card.php"); ?>
         <div class="footer">footer</div>
     </div>
 </body>
