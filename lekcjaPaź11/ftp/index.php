6f39ca97ee2a93049d285ec94d4a704f03e56da1
Gość <b>
2019-10-11 12:14:22 +0200

    new lesson

---
+++ lekcjaPaź11/ftp/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css" class="css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <img src="img1.png" a="logo" class="logo">
+            <h1>Tytuł</h1>
+            <ul>
+                <li><a href="#">Koszyki</a></li>
+                <li><a href="#">Klienci</a></li>
+                <li><a href="#">Produkty</a></li>
+            </ul>
+        </div>
+        <div class="sidebarLeft"></div>
+        <div class="main"></div>
+        <div class="sidebarRight"></div>
+        <div class="footer"></div>
+    </div>
+</body>
+</html>
