e03b6050ba8df7c351bbf0d3768fb57c09fcad53
Gość <b>
2019-12-04 11:25:44 +0100

    unfinished selectable

--- 19.12.04_kursy_walut/script.js
+++ 19.12.04_kursy_walut/script.js
@@ -1,7 +1,9 @@
 (
     function przelicznik() {
         const input = document.querySelector(".inputPLN");
+        const rateWindow = document.querySelector(".rateWindow");
         const rate = document.querySelector(".rate");
+        const currency = document.querySelector(".currency");
         const result = document.querySelector(".resultUSD");
         const button = document.querySelector(".calc");
 
@@ -9,11 +11,20 @@
 
         let xhr = new XMLHttpRequest;
         xhr.responseType = "json";
-        xhr.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/usd?format=json", true);
+        xhr.open("GET", "http://api.nbp.pl/api/exchangerates/tables/a?format=json", true);
         xhr.send();
         xhr.addEventListener("load", function() {
-            exchangeRate = xhr.response.rates[0].mid;
-            rate.innerText = exchangeRate + " PLN";
+            xhr.response[0].rates.forEach(function(item, i) {
+                let option = document.createElement("option");
+                option.value=i;
+                option.innerText = item.code;
+                currency.appendChild(option);
+            });
+        });
+
+        currency.addEventListener("change", function() {
+            exchangeRate = xhr.response[0].rates[currency.value].mid;
+            rate.innerText = exchangeRate;
         });
 
         button.addEventListener("click", function() {
